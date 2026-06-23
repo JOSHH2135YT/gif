8a2a12435fef11e6d4f2e595351966a10854a5e2
Joshua <joshuahabaku6@gmail.com>
2026-06-22 20:16:48 -0600

    Primer commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const formulario = document.getElementById("formulario");
+const entrada = document.getElementById("entrada");
+const contenedor = document.getElementById("contenedor");
+
+const key = "dKzsLPVggkh5g2sEYib4Mq9h0cvjxJynt17oxbPeJLowvfZvPUCTwhhB0lRgdWFH";
+
+const cargaGit = async (tema) => {
+  let accessPoint = `https://api.klipy.com/api/v1/${key}/gifs/search?q=${tema}`;
+  
+  try {
+    const respuesta = await fetch(accessPoint, {
+      method: "GET",
+    });
+    
+    let data = await respuesta.json();
+    let listaGit = data.data.data; 
+    
+    contenedor.innerHTML = "";
+
+    listaGit.map((imagen) => {
+      let gifUno = imagen.file.md.gif.url; 
+      
+      let miImagen = document.createElement("img"); 
+      miImagen.setAttribute('src', gifUno);
+      contenedor.append(miImagen);
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let texto = entrada.value;
+  cargaGit(texto);
+});
