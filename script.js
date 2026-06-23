32dd5c896d2ebd2c9a61be61d0804311c1ab7daf
Joshua <joshuahabaku6@gmail.com>
2026-06-22 20:24:56 -0600

    Se modificó la lógica principal y se corrigieron errores

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const formulario = document.getElementById("formulario");
 const entrada = document.getElementById("entrada");
 const contenedor = document.getElementById("contenedor");
 
-const key = "dKzsLPVggkh5g2sEYib4Mq9h0cvjxJynt17oxbPeJLowvfZvPUCTwhhB0lRgdWFH";
+const key = "YOs1VlLPycgVyQQNwVaqYmZlGpdDHQcvSzVsL8q5nP48g39bq8rRXp9gQXi1KoLd";
 
 const cargaGit = async (tema) => {
   let accessPoint = `https://api.klipy.com/api/v1/${key}/gifs/search?q=${tema}`;
